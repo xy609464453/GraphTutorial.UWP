49f2dae6707cb1d6a4741cb4c07903b4815c6790
IntInvoker <609464453@qq.com>
2023-04-06 12:49:10 +0800

    添加项目文件。

---
+++ MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Graph.Providers;
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace GraphTutorial
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Load OAuth settings
+            var oauthSettings = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView("OAuth");
+            var appId = oauthSettings.GetString("AppId");
+            var scopes = oauthSettings.GetString("Scopes");
+
+            if (string.IsNullOrEmpty(appId) || string.IsNullOrEmpty(scopes))
+            {
+                Notification.Show("Could not load OAuth Settings from resource file.");
+            }
+            else
+            {
+                // Configure MSAL provider  
+                MsalProvider.ClientId = appId;
+                MsalProvider.Scopes = new ScopeSet(scopes.Split(' '));
+
+                // Handle auth state change
+                ProviderManager.Instance.ProviderUpdated += ProviderUpdated;
+
+                // Navigate to HomePage.xaml
+                RootFrame.Navigate(typeof(HomePage));
+            }
+        }
+
+        private void ProviderUpdated(object sender, ProviderUpdatedEventArgs e)
+        {
+            var globalProvider = ProviderManager.Instance.GlobalProvider;
+            SetAuthState(globalProvider != null && globalProvider.State == ProviderState.SignedIn);
+            RootFrame.Navigate(typeof(HomePage));
+        }
+
+        private void SetAuthState(bool isAuthenticated)
+        {
+            (Application.Current as App).IsAuthenticated = isAuthenticated;
+
+            // Toggle controls that require auth
+            Calendar.IsEnabled = isAuthenticated;
+            NewEvent.IsEnabled = isAuthenticated;
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var invokedItem = args.InvokedItem as string;
+
+            switch (invokedItem.ToLower())
+            {
+                case "new event":
+                    throw new NotImplementedException();
+                    break;
+                case "calendar":
+                    throw new NotImplementedException();
+                    break;
+                case "home":
+                default:
+                    RootFrame.Navigate(typeof(HomePage));
+                    break;
+            }
+        }
+    }
+}
